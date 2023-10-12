d1b9e17e145ef58ca11966cf13821372805f3ca3
eddyzera <esilva644@gmail.com>
2023-10-12 13:46:54 -0300

    chore: use path mapping

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const Groups: React.FunctionComponent = () => {
+  return (
+    <View>
+      <Text style={{ color: 'black' }}>Hello world</Text>
+    </View>
+  )
+}
